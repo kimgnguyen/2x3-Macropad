4b084e1ad581ac5e87ad954b7e40e21b163a760b
Kim Gremillet-Nguyen <kim.g.nguyen@gmail.com>
2023-10-16 00:15:25 +0200

    Key assignments

--- keymaps/via/keymap.c
+++ keymaps/via/keymap.c
@@ -5,7 +5,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [0] = LAYOUT_ortho_2x3(
-       KC_A, KC_B, KC_C,
-       KC_D, KC_E, KC_F
+       LALT(KC_F4), KC_MUTE, KC_MPLY,
+       KC_MNXT, KC_MSTP, KC_MPRV
 	),
 };
